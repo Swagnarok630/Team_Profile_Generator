f59e16e196f0fd4c20cdb04038ab254dd7a86301
Swagnarok <darryl.le.630@gmail.com>
2022-08-14 03:18:46 -0700

    Finalized stylization

--- index.js
+++ index.js
@@ -159,13 +159,13 @@ function createHtml() {
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
+        <title>Team Profile</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
       </head>
-      <body>
-        <div class="p-4">
-        <h1 class="text-center mb-5">THE DREAM TEAM!!!</h1>
-        <div class="d-flex justify-content-evenly">
+      <body class="p-5 bg-dark">
+        <div class="p-5 mb-5 text-bg-dark">
+        <h1 class="text-center mb-5 fs-1 fw-bolder text-decoration-underline">THE DREAM TEAM!!!</h1>
+        <div class="gap-5 p-5 d-flex flex-wrap justify-content-evenly">
             ${employees.map(employee => employee.createCard()).join("\n")}
         </div>
         </div>
